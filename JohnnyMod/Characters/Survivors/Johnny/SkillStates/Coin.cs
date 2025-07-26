958a1d2c7a7b27f6a7144fbefbda20c3d094c5c4
PlNKWithAnL <willt655@gmail.com>
2025-07-26 13:51:48 -0400

    more progress?
    maybe?

--- JohnnyMod/Characters/Survivors/Johnny/SkillStates/Coin.cs
+++ JohnnyMod/Characters/Survivors/Johnny/SkillStates/Coin.cs
@@ -24,7 +24,7 @@ namespace JohnnyMod.Survivors.Johnny.SkillStates
 
             this.damageCoefficient = characterBody.damage * JohnnyStaticValues.coinDamageCoeffecient;
             this.projectileBaseSpeed = 32f;
-            this.detonationRadius = 100f;
+            this.detonationRadius = 2.5f;
             this.projectilePrefab = JohnnyAssets.coinProjectile;
             this.baseMinimumDuration = 0f;
             this.maxDistance = 100;
